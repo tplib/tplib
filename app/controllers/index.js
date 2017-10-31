5712e65ca102679ec7cb55fea95bf02d9a474769
tplib <tplib@protonmail.com>
2017-10-31 13:49:30 -0700

    query the DB for total records, show total in index view

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,14 +6,16 @@ const express = require('express'),
 router.get('/', (req, res) => {
   async.parallel({
     bookList: function (cb) { Book.find().sort({ 'title': 1}).exec(cb); },
+    bookCount: function (cb) { Book.count({}).exec(cb); },
     lastUploaded: function(cb) { Book.find().sort({ 'createdAt': -1 }).limit(3).exec(cb); },
     lastUpdated: function(cb)  { Book.findOne().sort({ 'updatedAt': -1 }).exec(cb); }
   }, (err, result) => {
     res.render('index', {
       title: 'tplib',
       book_list: result.bookList,
+      book_count: result.bookCount,
       last_uploaded: result.lastUploaded,
-      update_time: result.lastUpdated.updatedAt.toString()
+      last_updated: result.lastUpdated.updatedAt.toString()
     });
   });
 });
